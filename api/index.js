5139632ef5a12bf51a355f62367e80b8a4dc0588
Leonardo Troche <leo.m.troche@gmail.com>
2023-01-06 08:18:37 -0300

    Add new endpoints for api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static.module'
+
 import leaderboard from '../db/leaderboard.json'
 
 const app = new Hono()
@@ -15,5 +17,7 @@ app.get('/', (ctx) => {
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
 })
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
