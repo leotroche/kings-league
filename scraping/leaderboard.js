858512e0139efc565cb18b3dd3da5a1b495e0e6d
Leonardo Troche <leo.m.troche@gmail.com>
2023-01-08 10:14:57 -0300

    Refactor for leaderboard scraper

--- scraping/leaderboard.js
+++ scraping/leaderboard.js
@@ -1,41 +1,25 @@
-import * as cheerio from 'cheerio'
 import { writeDBFile, TEAMS, PRESIDENTS } from '../db/index.js'
+import { cleanText, scrape, URLS } from './utils.js'
 
-const URLS = {
-  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+const LEADERBOARD_SELECTORS = {
+  team: { selector: '.fs-table-text_3', typeOf: 'string' },
+  wins: { selector: '.fs-table-text_4', typeOf: 'number' },
+  losses: { selector: '.fs-table-text_5', typeOf: 'number' },
+  scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
+  concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
+  yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
+  redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
 }
 
-async function scrape(url) {
-  const resp = await fetch(url)
-  const html = await resp.text()
-  return cheerio.load(html)
+const getTeamFrom = ({ name }) => {
+  const { presidentId, ...restOfTeam } = TEAMS.find((team) => team.name === name)
+  const president = PRESIDENTS.find((president) => president.id === presidentId)
+  return { ...restOfTeam, president }
 }
-
-const cleanText = (text) => text.replace(/\t|\n|\s:/g, '').replace(/.*:/g, '')
 
 async function getLeaderBoard() {
   const $ = await scrape(URLS.leaderboard)
   const $row = $('table tbody tr')
-
-  const LEADERBOARD_SELECTORS = {
-    team: { selector: '.fs-table-text_3', typeOf: 'string' },
-    wins: { selector: '.fs-table-text_4', typeOf: 'number' },
-    loses: { selector: '.fs-table-text_5', typeOf: 'number' },
-    scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
-    concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
-    yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
-    redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
-  }
-
-  const getTeamFrom = ({ name }) => {
-    const { presidentId, ...restOfTeam } = TEAMS.find(
-      (team) => team.name === name
-    )
-    const president = PRESIDENTS.find(
-      (president) => president.id === presidentId
-    )
-    return { ...restOfTeam, president }
-  }
 
   const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTORS)
 
@@ -44,20 +28,15 @@ async function getLeaderBoard() {
   $row.each((_, el) => {
     const $el = $(el)
 
-    const leaderBoardEntries = leaderBoardSelectorEntries.map(
-      ([key, { selector, typeOf }]) => {
-        const rawValue = $el.find(selector).text()
-        const cleanedValue = cleanText(rawValue)
+    const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, { selector, typeOf }]) => {
+      const rawValue = $el.find(selector).text()
+      const cleanedValue = cleanText(rawValue)
+      const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
 
-        const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+      return [key, value]
+    })
 
-        return [key, value]
-      }
-    )
-
-    const { team: teamName, ...leaderboardForTeam } =
-      Object.fromEntries(leaderBoardEntries)
-
+    const { team: teamName, ...leaderboardForTeam } = Object.fromEntries(leaderBoardEntries)
     const team = getTeamFrom({ name: teamName })
 
     leaderboard.push({ ...leaderboardForTeam, team })
@@ -68,4 +47,4 @@ async function getLeaderBoard() {
 
 const leaderboard = await getLeaderBoard()
 
-await writeDBFile('leaderboard', leaderboard)
+writeDBFile('leaderboard', leaderboard)
